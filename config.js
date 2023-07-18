0d28380392e7b5e9d56536aa3608f6fe8677a4a7
01ABHAY10 <vishwakarmaabhay10@gmail.com>
2023-07-18 12:50:38 +0530

    .

--- config.js
+++ config.js
@@ -24,5 +24,16 @@ module.exports = {
     API_URL : 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=970d4991-d793-4b3b-8eac-5e6871d65a04&symbol=ETH&convert=USDT',
     MONGO_URL :'mongodb+srv://mew:mew12345@accounts.0h9engc.mongodb.net/?retryWrites=true&w=majority',
     PROVIDER : 'https://sepolia.infura.io/v3/2881ee9fd5b04bbab9da5fe590e1bb6c',
-    user
+    user,
+    KEY : [
+        89,  13,  33,   7,  14, 210, 137, 176,
+       179, 210, 251, 207, 159,  25,  70, 120,
+       214, 253,  53,  25, 110,  66,  66,  94,
+       221, 207, 174, 101, 209, 129, 189, 166
+     ],
+     iv : [
+        1,  45,  24, 106, 251, 129,
+      251, 114, 127,  34,  36,  24,
+       10, 105, 188,  50
+    ]
 }
