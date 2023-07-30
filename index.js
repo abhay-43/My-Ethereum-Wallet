2c74967e4637fa173779d9bef245c7a6953abe98
01ABHAY10 <vishwakarmaabhay10@gmail.com>
2023-07-30 09:26:08 +0530

    .

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const mongoose = require('mongoose');
-
+const cookieParser = require('cookie-parser');
 const { ethers } = require("ethers"); 
-
+const crypto = require('crypto');
 const {API_URL} = require('./config.js');
 const { MONGO_URL } = require("./config.js");
 const { user } = require("./config.js");
 const { PROVIDER } = require("./config.js");
 const { KEY, iv } = require("./config.js");
+const twelveHoursInMilliseconds = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
 const app = express();
 
 app.use(express.static("public"));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const encryptionKey = Buffer.from(KEY,'utf-8');
@@ -39,7 +41,11 @@ app.get("/",function(req,res){
 
 app.get("/homepage",function(req,res){
     //sending homepage file
+    const mew_id = req.cookies.mew_id;
+    const ip = req.ip;
+    if(Users[mew_id] && Users[mew_id].login && ip === Users[mew_id].ip)
     res.sendFile(__dirname + "/homepage.html");
+    else res.send("Unauthorised access !");
 
   })
 
@@ -69,6 +75,7 @@ app.get('/data', function(req, res) {
     https.get(url, function(response){
       response.on('data', function(data){
   
+       try{ 
         const ETHdata = JSON.parse(data);
         const P = ETHdata.data.ETH.quote.USDT.price;
         const Price = P.toFixed(2); //Price
@@ -80,7 +87,6 @@ app.get('/data', function(req, res) {
         const MarketCap = MC.toFixed(2); //Market cap
         const V24H = ETHdata.data.ETH.quote.USDT.volume_24h / 10e9;
         const Volume24H = V24H.toFixed(2); //Volume
-  
         // Create a JSON object with the data
         const dataToSend = {
           price: Price,
@@ -94,25 +100,40 @@ app.get('/data', function(req, res) {
         res.setHeader('Content-Type', 'application/json');
         // Send the JSON object as a response
         res.send(dataToSend);
+      }catch(error){
+        console.log(error);
+      }
       });
     });
   });
 
-  app.get('/mew_id',function(req,res){
-   res.setHeader('Content-Type','application/json');
-   res.send(Mew_ID);
-  //  console.log(Mew_ID);
+  app.get('/balance',async function(req,res){
+    const mew_id = req.cookies.mew_id;
+    const provider = new ethers.providers.JsonRpcProvider(PROVIDER);
+    try{
+      const account = await user.findOne({mew_id : mew_id});
+      const balance = await provider.getBalance(account.wallet);
+      const EtherBalance = ethers.utils.formatEther(balance);
+      const Ether = {
+        balance : (EtherBalance*1).toFixed(8)
+      }
+      // const ETH_USD = EtherBalance * currPrice;
+      // const ETHinUSD = ETH_USD.toFixed(4);
+      res.send(Ether);
+    }catch(error){
+      console.log(error);
+    }
   });
 
   //send transaction
    // bug : showing previous info
-   let txnUpdate;
    app.post('/send',async function(req,res){
     const to = req.body.toAddress;
     const amount = req.body.amount;
+    const mew_id = req.cookies.mew_id;
+    let txnUpdate;
     try{
-      const from = Mew_ID.add;
-      const txn = await sendTxn(from, to, amount);
+      const txn = await sendTxn(mew_id, to, amount);
       console.log("Txn done");
       if(txn.hash == undefined){
         // res.send("Transaction rejected due to " +txn.reason+".");
@@ -131,16 +152,17 @@ app.get('/data', function(req, res) {
       // res.send("Transaction cannot be done without connecting wallet!");
       txnUpdate = {
         type : -1,
-        update : "Transaction cannot be done without connecting wallet!"
+        update : "Transaction cannot be done!"
       }
     }
+    res.send(txnUpdate);
   });
 
    //Transaction update 
-   app.get('/txn-update',function(req,res){
-    res.setHeader('Content-Type', 'application/json');
-    res.send(txnUpdate);
-  });
+  //  app.get('/txn-update',function(req,res){
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.send(txnUpdate);
+  // });
    
   //create wallet function
   function CreateWallet(){
@@ -157,8 +179,9 @@ app.get('/data', function(req, res) {
   //function to send transaction
   async function sendTxn(from, to, amount){
     const provider = new ethers.providers.JsonRpcProvider(PROVIDER);
-    const wallet = await user.findOne({mew_id : from});
-    const signer = new ethers.Wallet(wallet.pvt_key,provider); 
+    const account = await user.findOne({mew_id : from});
+    const key = decryptData(account.pvt_key);
+    const signer = new ethers.Wallet(key,provider); 
     try{
       const txn = {
         to : to,
@@ -176,7 +199,7 @@ app.get('/data', function(req, res) {
   //function for encryption
    function encryptData (data){
       // Create a cipher using AES-256-CBC algorithm
-      const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, iv);
+      const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, IV);
       // Encrypt the data
       let encryptedData = cipher.update(data, 'utf-8', 'hex');
       encryptedData += cipher.final('hex');
@@ -186,30 +209,103 @@ app.get('/data', function(req, res) {
   //function for decryption
    function decryptData (data){
       // To decrypt the data
-      const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, iv);
+      const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, IV);
       // Decrypt the data
       let decryptedData = decipher.update(data, 'hex', 'utf-8');
       decryptedData += decipher.final('utf-8');
       return decryptedData;
   }
 
-
+const Users = {};
 app.post('/verify', async function(req,res){
   const mew_id = req.body.mew_id;
   try{
     const available = await user.findOne({mew_id : mew_id});
     if(available){
-
+      res.send(false);
     }else{
       const Wallet = CreateWallet();
       const wallet = Wallet.address;
       const pvt_key = encryptData(Wallet.privateKey);
       const mnemonic = encryptData(Wallet.mnemonic.phrase);
+      await user.create({mew_id,wallet,pvt_key,mnemonic});
+      res.cookie('mew_id', mew_id, { maxAge: twelveHoursInMilliseconds });
+      Users[mew_id] = {
+        login : true,
+        ip : req.ip
+      }
+      res.send(true);
     }
   }catch(error){
     console.log(error);
   }
 })
+
+app.post('/exist', async function(req,res){
+  const mew_id = req.body.mew_id;
+  try{
+    const available = await user.findOne({mew_id : mew_id});
+    if(available) res.send(true);
+    else res.send(false);
+  }catch(error){
+    console.log(error);
+  }
+})
+//rather perfect add hollow citizen please glide child cheese bread cousin journey
+app.post('/verify-mnemonic', async function(req,res){
+  const mnemonic = req.body.mnemonic;
+  const mew_id = req.body.mew_id;
+  try{
+    const account = await user.findOne({mew_id : mew_id});
+    const decryptedMnemonic = decryptData(account.mnemonic);
+    if(decryptedMnemonic === mnemonic){
+      res.cookie('mew_id', mew_id, { maxAge: twelveHoursInMilliseconds });
+      Users[mew_id] = {
+        login : true,
+        ip : req.ip
+      }
+      res.send(true);
+    }
+  }catch(error){
+    console.log(error);
+  }
+});
+
+app.get('/address', async function(req,res){
+  const mew_id = req.cookies.mew_id;
+  try{
+    const account = await user.findOne({mew_id : mew_id});
+    const address = {
+      add : account.wallet
+    }
+    res.send(address);
+  }catch(error){
+    console.log(error);
+  }
+});
+
+app.get('/info', async function(req,res){
+  const mew_id = req.cookies.mew_id;
+  try{
+    const account = await user.findOne({mew_id : mew_id});
+    const userInfo = {
+      mew_id : mew_id,
+      address : account.wallet,
+      privateKey : decryptData(account.pvt_key),
+      mnemonic : decryptData(account.mnemonic)
+    }
+    res.send(userInfo);
+  }catch(error){
+    console.log(error);
+  }
+});
+
+app.get('/logout', async function(req,res){
+  const mew_id = req.cookies.mew_id;
+    delete Users[mew_id];
+    res.clearCookie('mew_id');
+    res.send(true);
+});
 
 app.listen(3000,function(){
     console.log("server started !!");
--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,12 @@
 
 
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
+// const provider = new ethers.providers.Web3Provider(window.ethereum);
+// const signer = provider.getSigner();
 let currPrice;
 
 
 //connect button action
-async function connect(){
-  await provider.send("eth_requestAccounts",[]);
-  // const signer = provider.getSigner();
-  const walletAddress = await signer.getAddress();
-  console.log(walletAddress);
-  const data = {
-    wallet : walletAddress
-  }
-  localStorage.setItem('redirected',true);
-  const response = await fetch("http://localhost:3000/user",{
-      method : 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-      await response.text();
-      window.location.replace("/homepage");
-}
+
 
 //fetching json from server
 async function getData() {
@@ -50,12 +32,15 @@ async function getData() {
 }
 
 //fetching wallet address from database
-async function getMew_ID() {
+async function getInfo() {
   try{
-    const response = await fetch('http://localhost:3000/mew_id');
+    const response = await fetch('http://localhost:3000/info');
     const data = await response.json();
-    document.getElementById('disCntBtn').textContent = "Account";
-    document.getElementById('myaddress').value = data.add;
+    document.getElementById('acc_id').innerHTML ='<b>mew_id : </b><b style="color: green;">'+data.mew_id+"</b>";
+    document.getElementById('acc_wallet').value =data.address;
+    document.getElementById('acc_pvt').value =data.privateKey;
+    document.getElementById('acc_mne').placeholder =data.mnemonic;
+    
     }catch(error){
       console.log(error);
     }
@@ -64,14 +49,14 @@ async function getMew_ID() {
 //updating balance
 async function UpdateBalance(){
   try{
-    const response = await fetch('http://localhost:3000/mew_id');
+    const response = await fetch('http://localhost:3000/balance');
     const data = await response.json();
-    const balance = await provider.getBalance(data.add);
-    const EtherBalance = ethers.utils.formatEther(balance);
-    const Ether = (EtherBalance*1).toFixed(8);
-    const ETH_USD = EtherBalance * currPrice;
+    // const balance = await provider.getBalance(data.add);
+    // const EtherBalance = ethers.utils.formatEther(balance);
+    // const Ether = (EtherBalance*1).toFixed(8);
+    const ETH_USD = data.balance * currPrice;
     const ETHinUSD = ETH_USD.toFixed(4);
-    document.getElementById('balance').innerHTML = Ether+' ETH ≈ $'+ETHinUSD;
+    document.getElementById('balance').innerHTML = data.balance+' ETH ≈ $'+ETHinUSD;
   }catch(error){
     console.log(error);
   }
@@ -80,33 +65,45 @@ async function UpdateBalance(){
 //view transaction function
 async function viewTransaction(){
   try{
-    const response = await fetch('http://localhost:3000/mew_id');
+    const response = await fetch('http://localhost:3000/address');
     const data = await response.json();
+    console.log(data.add);
     window.open('https://sepolia.etherscan.io/address/'+data.add, '_blank');
   }catch(error){
     console.log(error);
   }
 }
 
-//fetching transaction update
-async function TxnUpdate(){
-  try{
-    const response = await fetch('http://localhost:3000/txn-update');
-    const data = await response.json();
-    if(data.type == 0){
-      var click = alert("Transaction rejected! \n Reason: "+data.update);
+//send Transaction function
+async function sendTransaction(){
+  const confirm = prompt("Write 'confirm' to sign transaction.");
+    if(confirm === 'confirm'){
+    const data = {
+      toAddress : document.getElementById('toAddress').value,
+      amount : document.getElementById('amount').value
+    }
+    const response = await fetch("http://localhost:3000/send",{
+        method : 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    const Responsedata = await response.json();
+    if(Responsedata.type == 0){
+      var click = alert("Transaction rejected! \n Reason: "+Responsedata.update);
       if(!click){
         location.reload();
       }
     }
-    else if(data.type == 1){
-      var click = alert("Transaction done! \n TxnHash: "+data.update);
+    else if(Responsedata.type == 1){
+      var click = alert("Transaction done! \n TxnHash: "+Responsedata.update);
       if(!click){
         location.reload();
       }
     }
-    else if(data.type == -1){
-      var click = alert(data.update);
+    else if(Responsedata.type == -1){
+      var click = alert(Responsedata.update);
       if(!click){
         location.reload();
       }
@@ -115,82 +112,226 @@ async function TxnUpdate(){
       var click = alert("Unknown Error Occured!");
       if(!click){
         location.reload();
-      }
-      
+      }  
+    }}
+    else{
+      alert("Transaction not confirmed...");
     }
-    console.log("hello");
-  }catch(error){
-    console.log(error);
+  }
+
+
+async function verify(){
+  const data = {
+    mew_id : document.getElementById('newWallet').value
+  }
+  const response = await fetch("http://localhost:3000/verify",{
+      method : 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const responseData = await response.json();
+    if(responseData){
+      window.location.href = '/homepage';
+    }
+    else alert("mew_id is already taken!\n Try another..."); 
+}
+
+const createWallet = document.getElementById('createWallet');
+if(createWallet){
+  createWallet.onclick = async function (){
+    await verify();
   }
 }
 
-
-
-window.onload = async function() {
-
-  //Redirect to homepage on connecting the wallet
-    // const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const cntBtn = document.getElementById('CntBtn');
-    if (cntBtn) {
-      cntBtn.onclick = connect;
-    }
-
-    //disconnect wallet hint 
-   if( document.getElementById('disCntBtn').textContent == "Connected"){
-    document.getElementById('disCntBtn').onclick = function (){
-      var click= alert('You can manually disconnect your wallet !\n\nFollow steps :\n\n Open metamask -> Tap "connected" -> Tap three dots (⋮) \n  -> Tap "Disconnect this account"');
-      if(click){}
-      else{
-        location.reload();
+async function login(){
+  const data = {
+    mew_id : document.getElementById('oldWallet').value
+  }
+  const exist = await fetch("http://localhost:3000/exist",{
+    method : 'POST',
+    headers : {
+      'Content-Type':'application/json'
+    },
+    body : JSON.stringify(data)
+  })
+  const responseData = await exist.json();
+  if(responseData){
+    const mnemonic = prompt("Paste your wallet mnemonic here:");
+    if(mnemonic != null || mnemonic != ""){
+      const data1 = {
+        mew_id : document.getElementById('oldWallet').value,
+        mnemonic : mnemonic
       }
+      const response = await fetch("http://localhost:3000/verify-mnemonic",{
+      method : 'POST',
+      headers : {
+      'Content-Type':'application/json'
+      },
+      body : JSON.stringify(data1)
+    })
+      const verified = await response.json();
+      if(verified) window.location.href = '/homepage';
+      else alert("Incorrect mnemonic! verification failed...");
     }
   }else{
-    document.getElementById('disCntBtn').onclick = connect;
-  } 
+    alert("#mew_id doesn't exist!");
+    location.reload();
+  }
+}
+const loginWallet = document.getElementById('loginWallet');
+if(loginWallet){
+  loginWallet.onclick = async function (){
+    await login();
+  }
+}
 
-    //updating data of ETH
-     if(localStorage.getItem('redirected') === 'true'){
-      await getMew_ID();
+if(window.location.href === 'http://localhost:3000/homepage'){
+  async function EthData(){
+    const publicKey = await getAddress();
+	    var myaddress = document.getElementById('myaddress');
+      myaddress.value = publicKey;
+    await getData();
+    await UpdateBalance();
+    setInterval(async function(){
       await getData();
       await UpdateBalance();
-      setInterval(async function(){
-        await getData();
-         await UpdateBalance(); 
-      },60000);
-      
-     }
-    
-    //Redirect to connect page on disconnecting the wallet
-    window.ethereum.on('accountsChanged', function (accounts) {
-        if (accounts.length === 0) {
-           localStorage.removeItem("redirected");
-           window.location.href = "/";
-        }
-    })
-    
-    // Copy to clipboard
-    document.getElementById('CopyButton').onclick = function () {
-	    var text = document.getElementById('myaddress');
-	    navigator.clipboard.writeText(text.value).then(function () {
+    },60000);
+  }
+  EthData();
+}
+
+const accountbtn = document.getElementById('account');
+if(accountbtn){
+  accountbtn.onclick = async function (){
+    await getInfo();
+  }
+}
+
+const ViewTxn = document.getElementById('view');
+if(ViewTxn){
+  ViewTxn.onclick = viewTransaction;
+}
+
+const copyMnemonic = document.getElementById('cpyMne');
+if(copyMnemonic){
+  copyMnemonic.onclick = function () {
+    var mnemonic = document.getElementById('acc_mne');
+    navigator.clipboard.writeText(mnemonic.placeholder).then(function () {
+    let button = document.getElementById('cpyMne')
+    button.classList.add('cpybtn');
+    button.innerHTML = "copied!";
+    setTimeout(function () { button.classList.remove('cpybtn'); button.innerHTML = "copy"; }, 3000);
+  })}
+}
+
+async function getAddress(){
+  const response = await fetch('http://localhost:3000/address');
+  const data = await response.json();
+  return data.add ;
+}
+
+  // Copy to clipboard
+  const receiveCopyBtn = document.getElementById('CopyButton');
+  if(receiveCopyBtn){
+    receiveCopyBtn.onclick = async function () {
+      const publicKey = await getAddress();
+	    var myaddress = document.getElementById('myaddress');
+      myaddress.value = publicKey;
+	    navigator.clipboard.writeText(myaddress.value).then(function () {
 	    let button = document.getElementById('CopyButton')
 	    button.classList.add('cpybtn');
 	    button.innerHTML = "copied!";
 	    setTimeout(function () { button.classList.remove('cpybtn'); button.innerHTML = "copy"; }, 1000);
 	  document.getElementById('CBR').click();
 	})}
+  }
 
-  //view Transactions
-  document.getElementById('view').onclick = viewTransaction;
-
-  //Transaction alert
-  document.getElementById('SendBtn').onclick = async function(){
+const sendBtn = document.getElementById('SendBtn');
+if(sendBtn){
+  sendBtn.onclick = async function (){
     document.getElementById('CBS').click();
-    // setTimeout(function(){alert('Transaction intiated ! \n Click "OK" and wait for confirmation.')},500);
-    setInterval( async function(){
-     await TxnUpdate();
-    },20000);
-  };
+   setTimeout(async function(){
+    await sendTransaction();
+   },300);
+  }
+}
 
+const logout = document.getElementById('logout');
+if(logout){
+  logout.onclick = async function (){
+    const response = await fetch('http://localhost:3000/address');
+    const data = await response.json();
+    if(data){
+      window.location.href = "/";
+    }
+  }
+}
+window.onload = async function() {
+
+  //Redirect to homepage on connecting the wallet
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // const cntBtn = document.getElementById('CntBtn');
+    // if (cntBtn) {
+    //   cntBtn.onclick = connect;
+    // }
+
+    //disconnect wallet hint 
+  //  if( document.getElementById('disCntBtn').textContent == "Connected"){
+  //   document.getElementById('disCntBtn').onclick = function (){
+  //     var click= alert('You can manually disconnect your wallet !\n\nFollow steps :\n\n Open metamask -> Tap "connected" -> Tap three dots (⋮) \n  -> Tap "Disconnect this account"');
+  //     if(click){}
+  //     else{
+  //       location.reload();
+  //     }
+  //   }
+  // }else{
+  //   document.getElementById('disCntBtn').onclick = connect;
+  // } 
+
+    //updating data of ETH
+    //  if(localStorage.getItem('redirected') === 'true'){
+    //   await getMew_ID();
+    //   await getData();
+    //   await UpdateBalance();
+    //   setInterval(async function(){
+    //     await getData();
+    //      await UpdateBalance(); 
+    //   },60000);
+      
+    //  }
+    
+    //Redirect to connect page on disconnecting the wallet
+    // window.ethereum.on('accountsChanged', function (accounts) {
+    //     if (accounts.length === 0) {
+    //        localStorage.removeItem("redirected");
+    //        window.location.href = "/";
+    //     }
+    // })
+    
+    // Copy to clipboard
+  //   document.getElementById('CopyButton').onclick = function () {
+	//     var text = document.getElementById('myaddress');
+	//     navigator.clipboard.writeText(text.value).then(function () {
+	//     let button = document.getElementById('CopyButton')
+	//     button.classList.add('cpybtn');
+	//     button.innerHTML = "copied!";
+	//     setTimeout(function () { button.classList.remove('cpybtn'); button.innerHTML = "copy"; }, 1000);
+	//   document.getElementById('CBR').click();
+	// })}
+
+  // //view Transactions
+  // document.getElementById('view').onclick = viewTransaction;
+
+  // //Transaction alert
+  // document.getElementById('SendBtn').onclick = async function(){
+  //   document.getElementById('CBS').click();
+  //   // setTimeout(function(){alert('Transaction intiated ! \n Click "OK" and wait for confirmation.')},500);
+  //   setInterval( async function(){
+  //    await TxnUpdate();
+  //   },20000);
+  // };
   };
 
   
